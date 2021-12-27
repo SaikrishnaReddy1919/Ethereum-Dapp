4a942479d1d29b630ef38c6eea93931da54d17e7
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-27 21:39:04 +0530

    fetch greeting logic

--- ethereum-dapp/src/App.js
+++ ethereum-dapp/src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greetor from "./artifacts/contracts/Greeter.sol/Greeter.json"
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+
+  const [greeting, setgreeting] = useState();
+
+  async function requestAccount() {
+
+  }
+
+  async function fetchGreeting() {
+    if(window.ethereum !== undefined){
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greetor.abi, provider)
+      try {
+        const data = await contract.greet();
+        console.log('Data : ', data)
+      } catch (error) {
+        console.log("Error : ", error)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
